af8bc1c8c55464e523287651a517d294a973fadf
hernandoagudelo <heagma@live.com>
2022-07-04 21:29:45 +0800

    fix: modify control loop

--- internal/networkingandcontdelivery/vpc/subnet.go
+++ internal/networkingandcontdelivery/vpc/subnet.go
@@ -2,46 +2,55 @@ package vpc
 
 import (
 	"core-infra/config"
+	"fmt"
 	"github.com/pulumi/pulumi-aws-native/sdk/go/aws/ec2"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 var (
-	//subnetLogicalNames []string
-	subnetOutputNames []string
+	subnetLogicalNames []string
+	subnetOutputNames  []string
 )
 
 func CreateSubnet(ctx *pulumi.Context) error {
+
 	//Load configurations
 	cd := config.NewConfig(ctx)
+
 	configSubnetNames := cd.SubnetNames
 	configAz := cd.Az
+
+	configEnv := cd.Env
+	configRegion := cd.RegionAlias
 
 	//we are setting 2 subnets (private and public) per az in the 3 az available.
 
 	//Loop through the exported vpc resources
-	for _, name := range vpcOutputNames {
-		vpcResult, err := ec2.LookupVPC(ctx, &ec2.LookupVPCArgs{
-			VpcId: name,
-		})
 
-		if err != nil {
-			return err
-		}
+	for _, vpc := range VPC {
 
-		for index, subnetName := range configSubnetNames {
-			//Define the names of each subnet resource. This is for logical resources (get id, arn etc.),
-			//physical names have a random suffix and also Create names (adding "-out" at the end of the vpc names)
-			//for ids to later be used as keys when exporting the resource
+		//vpcResult := ec2.LookupVPCOutput(ctx, ec2.LookupVPCOutputArgs{
+		//	VpcId: vpc.VpcId,
+		//})
 
-			//subnetLogicalNames = append(subnetLogicalNames, config.FormatName(configEnv, configRegion, vpcName))
-			subnetOutputNames = append(subnetOutputNames, config.FormatName(subnetName, "out"))
+		for index, azName := range configAz {
+			fmt.Printf("the AZ are: %[1]s\n", azName)
 
-			for _, azName := range configAz {
+			for _, subnetName := range configSubnetNames {
+				fmt.Println(subnetName)
+
+				//Define the names of each subnet resource. This is for logical resources (get id, arn etc.),
+				//physical names have a random suffix and also Create names (adding "-out" at the end of the vpc names)
+				//for ids to later be used as keys when exporting the resource
+
+				subnetLogicalNames = append(subnetLogicalNames, config.FormatName(configEnv, configRegion, azName, subnetName))
+				subnetOutputNames = append(subnetOutputNames, config.FormatName(subnetName, "out"))
+
+				fmt.Printf("logical names %[1]v\n", subnetLogicalNames[index])
 
 				//Create our subnets taking the exported vpc id as parameters and the az taken from the config settings
-				subnet, err := ec2.NewSubnet(ctx, subnetName, &ec2.SubnetArgs{
-					AvailabilityZone: pulumi.String(azName), VpcId: pulumi.String(*vpcResult.VpcId),
+				subnet, err := ec2.NewSubnet(ctx, subnetLogicalNames[index], &ec2.SubnetArgs{
+					AvailabilityZone: pulumi.String(azName), VpcId: vpc.VpcId,
 				})
 
 				if err != nil {
