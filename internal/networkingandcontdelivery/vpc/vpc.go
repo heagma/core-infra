c3df88f582480ec965b61cb56ed454141837577d
hernandoagudelo <heagma@live.com>
2022-07-04 21:29:01 +0800

    feat: add new variable to keep all the values of type *VPC

--- internal/networkingandcontdelivery/vpc/vpc.go
+++ internal/networkingandcontdelivery/vpc/vpc.go
@@ -8,6 +8,7 @@ import (
 )
 
 var (
+	VPC             []*ec2.VPC
 	vpcLogicalNames []string
 	vpcOutputNames  []string
 )
@@ -28,29 +29,36 @@ func CreateVpc(ctx *pulumi.Context) error {
 		return errors.New("mismatch between the amount of vpc and the cidr configured")
 	}
 
-	for index, vpcName := range configVpcNames {
+	for _, vpcName := range configVpcNames {
 		//Define the names of each vpc resource. This is for logical resources (get id, arn etc.), physical names have a
-		//random suffix and also Create names (adding "-out" at the end of the vpc names) for ids to later be used as
-		//keys when exporting the resource
+		//random suffix and also Create names (adding "-out" at the end of the vpc names) for the outputs names
 		vpcLogicalNames = append(vpcLogicalNames, config.FormatName(configEnv, configRegion, vpcName))
 		vpcOutputNames = append(vpcOutputNames, config.FormatName(vpcName, "out"))
 
+	}
+
+	for index, vpcLogicalName := range vpcLogicalNames {
+
 		//Create our VPC resources
-		vpc, err := ec2.NewVPC(ctx, vpcName, &ec2.VPCArgs{
+		vpc, err := ec2.NewVPC(ctx, vpcLogicalName, &ec2.VPCArgs{
 			CidrBlock:          pulumi.String(configVpcCidr[index]),
 			EnableDnsHostnames: pulumi.Bool(true),
 			Tags: ec2.VPCTagArrayInput(
 				ec2.VPCTagArray{
-					ec2.VPCTagArgs{Key: pulumi.String(config.InitialTags.Name), Value: pulumi.String(vpcName)},
+					ec2.VPCTagArgs{Key: pulumi.String(config.InitialTags.Name), Value: pulumi.String(vpcLogicalName)},
 					ec2.VPCTagArgs{Key: pulumi.String(config.InitialTags.Env), Value: pulumi.String(configEnv)},
 				},
 			),
 		})
+
+		VPC = append(VPC, vpc)
+
 		if err != nil {
 			return err
 		}
 
 		ctx.Export(vpcOutputNames[index], vpc.ID())
 	}
+
 	return nil
 }
