16c1a20aa5b678c4ed37884464d58ccb3cb1dc95
Hernando Agudelo Madero <107454660+heagma@users.noreply.github.com>
2022-07-08 09:34:16 +0800

    fix:  remove unique names

--- initialization.sh
+++ initialization.sh
@@ -4,4 +4,4 @@
 curl -fsSL https://get.pulumi.com | sh
 
 ##Create an S3 Bucket to store the remote state
-aws s3api create-bucket --bucket aws-pulumi-core-infra-state --region ap-east-1 --create-bucket-configuration LocationConstraint=ap-east-1
+aws s3api create-bucket --bucket <add-bucket-name-here> --region <add-region-here> --create-bucket-configuration LocationConstraint=<add-same-region-here>
