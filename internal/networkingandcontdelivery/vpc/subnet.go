a6b312148ebbe29dc5246b85ad69c4bd2ab5f5aa
hernandoagudelo <heagma@live.com>
2022-07-13 23:46:43 +0800

    feat: add natgateway call out of subnet creation

--- internal/networkingandcontdelivery/vpc/subnet.go
+++ internal/networkingandcontdelivery/vpc/subnet.go
@@ -4,9 +4,7 @@ import (
 	"core-infra/config"
 	"fmt"
 	"github.com/pulumi/pulumi-aws-native/sdk/go/aws/ec2"
-	cec2 "github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ec2"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-	"strings"
 )
 
 //CustomSubnet struct is a composite (not inheritance) struct that embed the ec2.Subnet struct
@@ -17,23 +15,20 @@ type CustomSubnet struct {
 	logicalName string
 }
 
-func CreateSubnet(ctx *pulumi.Context, VPCs []*ec2.VPC) ([]*CustomSubnet, error) {
+func CreateSubnet(ctx *pulumi.Context, cfg *config.DataConfig, VPCs []*ec2.VPC) ([]*CustomSubnet, error) {
 	var (
 		Subnets            []*CustomSubnet
 		subnetLogicalNames []string
 		subnetOutputNames  []string
 	)
 
-	//Load configurations
-	cd := config.NewConfig(ctx)
+	configEnv := cfg.Env
+	configRegion := cfg.RegionAlias
 
-	configEnv := cd.Env
-	configRegion := cd.RegionAlias
-
-	//Assign configurations related only to vpc
-	configSubnetTypes := cd.SubnetTypes
-	configSubnetCidrs := cd.SubnetCidrs
-	configAz := cd.Az
+	//Assign configurations related only to subnets
+	configSubnetTypes := cfg.SubnetTypes
+	configSubnetCidrs := cfg.SubnetCidrs
+	configAz := cfg.Az
 
 	//load initial tags
 	initialTags := config.NewInitialTags()
@@ -45,7 +40,7 @@ func CreateSubnet(ctx *pulumi.Context, VPCs []*ec2.VPC) ([]*CustomSubnet, error)
 			for _, azName := range configAz {
 
 				//Define the names of each subnet resource. This is for logical resources (get id, arn etc.),
-				//Outputs names is to later be used as keys in case another project/stack needs it
+				//Outputs names is to later be used as keys in case another project/stack needs it.
 
 				subnetLogicalNames = append(subnetLogicalNames, config.FormatName(configEnv, configRegion, azName, "sn", subnetType))
 				subnetOutputNames = append(subnetOutputNames, config.FormatName(azName, subnetType, "out"))
@@ -79,43 +74,6 @@ func CreateSubnet(ctx *pulumi.Context, VPCs []*ec2.VPC) ([]*CustomSubnet, error)
 				Subnets = append(Subnets, &CustomSubnet{subnet, subnetLogicalName})
 
 				ctx.Export(subnetLogicalName, subnet.ID())
-
-				//Attach a Nat gateway to each public subnet
-				if strings.HasSuffix(subnetLogicalName, "public") {
-
-					//Create an EIP first to be associated with the NATGateway
-					//Lets give our eip a temp name to be used as a logical name
-					eipTempName := config.FormatName(subnetLogicalName, "nat", "eip")
-
-					eip, err := cec2.NewEip(ctx, eipTempName, &cec2.EipArgs{
-						Tags: pulumi.StringMap{
-							initialTags.Name: pulumi.String(eipTempName),
-							initialTags.Env:  pulumi.String(configEnv),
-						},
-					})
-
-					if err != nil {
-						return nil, fmt.Errorf("CreateSubnet: failed creating eip resource %[1]w", err)
-					}
-
-					_, err = ec2.NewNatGateway(ctx, fmt.Sprintf("%[1]s-nat", subnetLogicalName), &ec2.NatGatewayArgs{
-						SubnetId:     subnet.ID(),
-						AllocationId: eip.ID(),
-						Tags: ec2.NatGatewayTagArrayInput(
-							ec2.NatGatewayTagArray{
-								ec2.NatGatewayTagArgs{
-									Key:   pulumi.String(initialTags.Name),
-									Value: pulumi.String(fmt.Sprintf("%[1]s-nat", subnetLogicalName)),
-								},
-								ec2.NatGatewayTagArgs{
-									Key: pulumi.String(initialTags.Env), Value: pulumi.String(configEnv),
-								},
-							}),
-					})
-					if err != nil {
-						return nil, fmt.Errorf("CreateSubnet: failed creating nat gateway resource %[1]w", err)
-					}
-				}
 
 				break
 
