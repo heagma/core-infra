a31e77c68c99f3e90dfaef352fc584b405ef229c
hernandoagudelo <heagma@live.com>
2022-07-21 23:34:17 +0800

    refactor: remove slice type vpc block

--- internal/networkingandcontdelivery/vpc/subnet.go
+++ internal/networkingandcontdelivery/vpc/subnet.go
@@ -15,7 +15,7 @@ type CustomSubnet struct {
 	logicalName string
 }
 
-func CreateSubnet(ctx *pulumi.Context, cfg *config.DataConfig, VPCs []*ec2.VPC) ([]*CustomSubnet, error) {
+func CreateSubnet(ctx *pulumi.Context, cfg *config.DataConfig, vpc *ec2.VPC) ([]*CustomSubnet, error) {
 	var (
 		Subnets            []*CustomSubnet
 		subnetLogicalNames []string
@@ -25,53 +25,49 @@ func CreateSubnet(ctx *pulumi.Context, cfg *config.DataConfig, VPCs []*ec2.VPC) 
 	//load initial tags
 	initialTags := config.NewInitialTags()
 
-	//Loop through the exported vpc resources
-	for _, vpc := range VPCs {
-		//we are setting 2 subnets (private and public) per az in the 3 az available.
-		for _, subnetType := range cfg.SubnetTypes {
-			for _, azName := range cfg.Az {
+	//we are setting 2 subnets (private and public) per az in the 3 az available.
+	for _, subnetType := range cfg.SubnetTypes {
+		for _, azName := range cfg.Az {
 
-				//Define the names of each subnet resource. This is for logical resources (get id, arn etc.),
-				//Outputs names is to later be used as keys in case another project/stack needs it.
+			//Define the names of each subnet resource. This is for logical resources (get id, arn etc.),
+			//Outputs names is to later be used as keys in case another project/stack needs it.
 
-				subnetLogicalNames = append(subnetLogicalNames, config.FormatName(cfg.Env, cfg.RegionAlias, azName, "sn", subnetType))
-				subnetOutputNames = append(subnetOutputNames, config.FormatName(azName, subnetType, "out"))
-			}
+			subnetLogicalNames = append(subnetLogicalNames, config.FormatName(cfg.Env, cfg.RegionAlias, azName, "sn", subnetType))
+			subnetOutputNames = append(subnetOutputNames, config.FormatName(azName, subnetType, "out"))
 		}
+	}
 
-		//Loop through the logical names to create named subnets within each AZ
-		for i, subnetLogicalName := range subnetLogicalNames {
-			for _, azName := range cfg.Az {
-				//Create our subnets taking the exported vpc id as parameters and the az taken from the config settings
-				//here we assign the logical names at the same time unlike when creating our VPC
-				subnet, err := ec2.NewSubnet(ctx, subnetLogicalName, &ec2.SubnetArgs{
-					AvailabilityZone: pulumi.String(azName), VpcId: vpc.VpcId,
-					CidrBlock: pulumi.String(cfg.SubnetCidrs[i]),
-					Tags: ec2.SubnetTagArrayInput(
-						ec2.SubnetTagArray{
-							ec2.SubnetTagArgs{
-								Key: pulumi.String(initialTags.Name), Value: pulumi.String(subnetLogicalName),
-							},
-							ec2.SubnetTagArgs{
-								Key: pulumi.String(initialTags.Env), Value: pulumi.String(cfg.Env),
-							},
+	//Loop through the logical names to create named subnets within each AZ
+	for i, subnetLogicalName := range subnetLogicalNames {
+		for _, azName := range cfg.Az {
+			//Create our subnets taking the exported vpc id as parameters and the az taken from the config settings
+			//here we assign the logical names at the same time unlike when creating our VPC
+			subnet, err := ec2.NewSubnet(ctx, subnetLogicalName, &ec2.SubnetArgs{
+				AvailabilityZone: pulumi.String(azName), VpcId: vpc.VpcId,
+				CidrBlock: pulumi.String(cfg.SubnetCidrs[i]),
+				Tags: ec2.SubnetTagArrayInput(
+					ec2.SubnetTagArray{
+						ec2.SubnetTagArgs{
+							Key: pulumi.String(initialTags.Name), Value: pulumi.String(subnetLogicalName),
 						},
-					),
-				})
+						ec2.SubnetTagArgs{
+							Key: pulumi.String(initialTags.Env), Value: pulumi.String(cfg.Env),
+						},
+					},
+				),
+			})
 
-				if err != nil {
-					return nil, fmt.Errorf("CreateSubnet: failed creating subnet resource %[1]w", err)
-				}
-
-				Subnets = append(Subnets, &CustomSubnet{subnet, subnetLogicalName})
-
-				ctx.Export(subnetLogicalName, subnet.ID())
-
-				break
-
+			if err != nil {
+				return nil, fmt.Errorf("CreateSubnet: failed creating subnet resource %[1]w", err)
 			}
-		}
 
+			Subnets = append(Subnets, &CustomSubnet{subnet, subnetLogicalName})
+
+			ctx.Export(subnetLogicalName, subnet.ID())
+
+			break
+
+		}
 	}
 
 	return Subnets, nil
